24037665169b1502ca58f73cd63085288c7e2e51
Hans-Peter Deifel <hpdeifel@gmx.de>
2014-09-25 16:26:27 +0200

    Change package description to one line

--- synosaurus-pkg.el
+++ synosaurus-pkg.el
@@ -1,12 +1,4 @@
 (define-package "synosaurus" "0.9"
-  "An extensible thesaurus supporting lookup and substitution.
-
-You can choose between multiple backends. Current backends include
-wordnet and openthesaurus, but it's easy to add your own.
-
-Use `synosaurus-lookup' for lookup and `synosaurus-choose-and-replace'
-to replace the word under cursor.
-
-Customization can be done by M-x customize-group synosaurus"
+  "An extensible thesaurus supporting lookup and substitution."
 
   '((cl-lib "1.0")))
